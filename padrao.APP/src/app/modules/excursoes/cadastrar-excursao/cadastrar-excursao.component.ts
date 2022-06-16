f7e694e76f86d169fb7ba0352ecaf3b3bce90d7b
Daniel Lucas <danidldl_@hotmail.com>
2022-06-16 11:44:51 -0300

    primeiro dados form

--- padrao.APP/src/app/modules/excursoes/cadastrar-excursao/cadastrar-excursao.component.ts
+++ padrao.APP/src/app/modules/excursoes/cadastrar-excursao/cadastrar-excursao.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/material/core';
 import { MY_FORMATS } from 'app/core/functions/date-format';
 import { scrollFactory } from 'app/core/functions/scroll-factory';
+import { ToastService } from 'app/core/services/toast.service';
 
 @Component({
     selector: 'app-cadastrar-excursao',
@@ -36,7 +37,10 @@ import { scrollFactory } from 'app/core/functions/scroll-factory';
 export class CadastrarExcursaoComponent implements OnInit {
     public form: FormGroup;
 
-    constructor(private _formBuilder: FormBuilder) {}
+    constructor(
+        private _formBuilder: FormBuilder,
+        private _toastService: ToastService
+    ) {}
 
     ngOnInit() {
         this.form = this._formBuilder.group({
@@ -46,6 +50,8 @@ export class CadastrarExcursaoComponent implements OnInit {
                 dataFim: [null, Validators.required],
                 dataSaida: [null, Validators.required],
                 dataRetorno: [null, Validators.required],
+                horaEmbarque: [null],
+                horaRetorno: [null],
             }),
             step2: this._formBuilder.group({
                 valorAdulto: [null, Validators.required],
@@ -60,5 +66,80 @@ export class CadastrarExcursaoComponent implements OnInit {
                 onibusMotoristas: [null],
             }),
         });
+    }
+
+    public setDatasSelecionadas(): void {
+        const dataInicio = this.form.get('step1').get('dataIncio');
+        const dataFim = this.form.get('step1').get('dataFim');
+        const dataSaida = this.form.get('step1').get('dataSaida');
+        const dataRetorno = this.form.get('step1').get('dataRetorno');
+        //Validar data saida
+        if (dataInicio.value != null && dataSaida.value == null) {
+            let dataS = new Date(dataInicio.value);
+            dataSaida.setValue(new Date(dataS.getTime() - 86400000));
+        } else if (
+            dataInicio.value != null &&
+            new Date(dataInicio.value).getTime() <
+                new Date(dataSaida.value).getTime()
+        ) {
+            let dataS = new Date(dataInicio.value);
+            dataSaida.setValue(new Date(dataS.getTime()));
+        }
+        //Validar data retorno
+        if (dataFim.value != null && dataRetorno.value == null) {
+            let dataF = new Date(dataFim.value);
+            dataRetorno.setValue(new Date(dataF.getTime()));
+        } else if (
+            dataFim.value != null &&
+            new Date(dataFim.value).getTime() <
+                new Date(dataRetorno.value).getTime()
+        ) {
+            let dataF = new Date(dataFim.value);
+            dataRetorno.setValue(new Date(dataF.getTime()));
+        } else if (
+            new Date(dataFim.value).getTime() >
+            new Date(dataRetorno.value).getTime()
+        ) {
+            let dataF = new Date(dataFim.value);
+            dataRetorno.setValue(new Date(dataF.getTime()));
+        }
+    }
+
+    public validarDataSaida(): boolean {
+        const dataInicio = this.form.get('step1').get('dataIncio');
+        const dataSaida = this.form.get('step1').get('dataSaida');
+        if (dataInicio.value != null && dataSaida.value != null) {
+            if (
+                new Date(dataInicio.value).getTime() <
+                new Date(dataSaida.value).getTime()
+            ) {
+                this._toastService.mensagemWarning(
+                    'Data saída maior que a data ínicio.'
+                );
+                let dataF = new Date(dataInicio.value);
+                dataSaida.setValue(new Date(dataF.getTime()));
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public validarDataRetorno(): boolean {
+        const dataFim = this.form.get('step1').get('dataFim');
+        const dataRetorno = this.form.get('step1').get('dataRetorno');
+        if (dataFim.value != null && dataRetorno.value != null) {
+            if (
+                new Date(dataFim.value).getTime() >
+                new Date(dataRetorno.value).getTime()
+            ) {
+                this._toastService.mensagemWarning(
+                    'Data retorno menor que a data fim.'
+                );
+                let dataF = new Date(dataFim.value);
+                dataRetorno.setValue(new Date(dataF.getTime()));
+                return false;
+            }
+        }
+        return true;
     }
 }
